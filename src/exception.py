95c7e3d7c81d9ba916faba3b18332659fc32d8d7
Feroz-Shah-coder <githubm22@gmail.com>
2026-07-16 16:03:00 +0500

    Porject Strure , loggin and exceptio

---
+++ src/exception.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+def error_message_details(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name  = exc_tb.tb_frame.f_code.co_filename
+    error = "Error occurse diung pyton scirpt name [{0}], line number [{1}], error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+
+import sys
+
+# Custom exception class
+class CustomException(Exception):
+
+    # Constructor
+    def __init__(self, error_message, error_detail: sys):
+        # Call the parent Exception class constructor
+        super().__init__(error_message)
+
+        # Store the detailed error message
+        self.error_message = error_message_details(
+            error_message,
+            error_detail=error_detail
+        )
+
+    # String representation of the exception
+    def __str__(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by Zero Error ")
+        raise CustomException(e, sys)
